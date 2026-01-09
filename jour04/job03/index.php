7cf6868e50a50a518bd403c0a3be4821ba710a1c
lorenzo-brissardnavarro <lorenzo.brissardnavarro@laplateforme.io>
2026-01-09 10:32:41 +0100

    feat : job03 et job04

---
+++ jour04/job03/index.php
@@ -0,0 +1,30 @@
+<?php
+
+$count = 0;
+if(!empty($_POST)){
+    foreach ($_POST as $key => $value) {
+        $count++;
+        echo $key . " => " . $value . "<br>";
+    }
+    echo "Nombre d'arguments : " . $count;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Méthode Post</title>
+</head>
+<body>
+    <main>
+        <form action="" method="POST">
+            <input type="email" name="email" placeholder="Adresse mail">
+            <input type="password" name="pwd" placeholder="Mot de passe">
+            <input type="submit" value="Envoyer le formulaire">
+        </form>
+    </main>
+</body>
+</html>
